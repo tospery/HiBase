0a3c6ca70dbf0241132c89eac125dc0886b91f87
tospery <tospery@gmail.com>
2025-01-13 20:29:17 +0800

    1.0.2

---
+++ Example/HiBase_Example/HiBase_ExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  HiBase_ExampleApp.swift
+//  HiBase_Example
+//
+//  Created by 杨建祥 on 2025/1/13.
+//
+
+import SwiftUI
+
+@main
+struct HiBase_ExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
--- HiBase.podspec
+++ HiBase.podspec
@@ -1,6 +1,6 @@
 Pod::Spec.new do |s|
   s.name             = 'HiBase'
-  s.version          = '1.0.1'
+  s.version          = '1.0.2'
   s.summary          = 'Base layer.'
   s.description      = <<-DESC
 						Base layer using Swift.
